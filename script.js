35fae32184a90bc88ec6f138ee3dd303f9af9134
Amanda <amwu0001@stud.kea.dk>
2024-01-07 16:57:39 +0000

    opstart af portfolio sider og forside done

--- script.js
+++ script.js
@@ -1,26 +1,28 @@
 function myFunction(x) {
+  console.log("myfunc");
   x.classList.toggle("change");
 }
 
 const burger = document.querySelector(".burger");
 const nav = document.querySelector("nav");
-const menu = document.querySelector(".menu");
-const links = document.querySelectorAll(".menu li a");
-
-/* burger til kryds symbol ved klik (har taget udgangspunkt i eksempel fra w3schools.com) */
+const menu = document.querySelector(".navbar");
+const links = document.querySelectorAll(".navbar li a");
 
 burger.addEventListener("click", () => {
+  console.log("1");
   burger.classList.toggle("active");
   nav.classList.toggle("active");
+  document.querySelector("main").classList.toggle("hide");
 });
 
 links.forEach((link) => {
+  console.log("2");
   link.addEventListener("click", () => {
     burger.classList.remove("active");
-    nav.classList.remove("active");
   });
-});
 
+  /* fade in */
+});
 window.addEventListener("load", sidenVises);
 
 function sidenVises() {
